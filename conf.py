29f8224d12c3e169f8854dd96bedc3cec6e218a7
narasimhasaipragada <68434794+narasimhasaipragada@users.noreply.github.com>
2020-07-18 14:33:01 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
+project = 'Attendence management system'
 copyright = '2020, APSSDC'
-author = 'APSSDC'
+author = 'narasimha'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
